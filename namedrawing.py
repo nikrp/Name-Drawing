b8b2b2abc8fec2816324658e0f5a140bdd4dcc8b
nikrp <76831568+nikrp@users.noreply.github.com>
2021-08-05 21:20:13 -0700

    thing
    added decor

--- namedrawing.py
+++ namedrawing.py
@@ -21,19 +21,20 @@ def drawCircleSpiral():
         t.left(30)
 
 def drawNikhil():
-    colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow', 'lime', 'magenta', 'sea green', 'white']
-    turtle.bgcolor('black')
-    
+   
     # Set Things Up
+    colors = ['blue', 'red', 'orange', 'yellow', 'lime', 'cyan', 'violet', 'purple', 'sea green', 'white']
+    turtle.bgcolor('black')
     t = turtle.Pen()
     t.width(15)
     t.up()
     t.left(180)
     t.forward(300)
     t.down()
+    t.speed(0)
 
     # N
-    t.pencolor(random.choice(colors))
+    t.pencolor(colors[1])
     t.right(90)
     t.forward(100)
     t.right(135)
@@ -42,7 +43,7 @@ def drawNikhil():
     t.forward(100)
 
     # I
-    t.pencolor(random.choice(colors))
+    t.pencolor(colors[2])
     t.up()
     t.right(90)
     t.forward(30)
@@ -55,7 +56,7 @@ def drawNikhil():
     t.forward(70)
 
     # K
-    t.pencolor(random.choice(colors))
+    t.pencolor(colors[3])
     t.up()
     t.left(90)
     t.forward(30)
@@ -71,7 +72,7 @@ def drawNikhil():
     t.left(45)
 
     # H
-    t.pencolor(random.choice(colors))
+    t.pencolor(colors[4])
     t.up()
     t.forward(30)
     t.down()
@@ -86,7 +87,7 @@ def drawNikhil():
     t.forward(100)
 
     # I
-    t.pencolor(random.choice(colors))
+    t.pencolor(colors[5])
     t.up()
     t.right(90)
     t.forward(30)
@@ -99,7 +100,7 @@ def drawNikhil():
     t.forward(70)
 
     # L
-    t.pencolor(random.choice(colors))
+    t.pencolor(colors[6])
     t.left(90)
     t.up()
     t.forward(30)
@@ -111,7 +112,7 @@ def drawNikhil():
     t.right(90)
 
     # !
-    t.pencolor(random.choice(colors))
+    t.pencolor(colors[7])
     t.up()
     t.forward(80)
     t.down()
@@ -123,9 +124,73 @@ def drawNikhil():
     t.forward(1)
 
     # Decorations
-       
-# Functions to run.
+    t.up()
+    t.forward(30)
+    t.right(90)
+    t.down()
+
+    # Lines Below Name
+    t.pencolor("turquoise")
+    t.forward(430)
+    t.up()
+    t.pencolor("magenta")
+    t.left(90)
+    t.forward(30)
+    t.left(90)
+    t.forward(40)
+    t.down()
+    t.forward(350)
+    t.up()
+    t.pencolor("red")
+    t.right(90)
+    t.forward(30)
+    t.right(90)
+    t.forward(40)
+    t.down()
+    t.forward(270)
+    t.up()
+    t.pencolor("yellow")
+    t.left(90)
+    t.forward(30)
+    t.left(90)
+    t.forward(40)
+    t.down()
+    t.forward(190)
+
+    # Balloons
+    t.up()
+    t.forward(210)
+    t.left(90)
+    t.up()
+    t.forward(220)
+    t.pencolor("red")
+    t.width(50)
+    t.down()
+    t.forward(0)
+    t.width(10)
+    t.up()
+    t.backward(27)
+    t.down()
+    t.pencolor("white")
+    t.backward(73)
+    t.up()
+    t.left(90)
+    t.forward(610)
+    t.right(90)
+    t.forward(100)
+    t.width(50)
+    t.pencolor("violet")
+    t.down()
+    t.forward(0)
+    t.width(10)
+    t.up()
+    t.backward(27)
+    t.down()
+    t.pencolor("blue")
+    t.backward(73)
+    
+# Functions To Run
 if __name__ == "__main__":
-#    drawSquareSpiral()
+    drawSquareSpiral()
 #    drawCircleSpiral()
-    drawNikhil()
+#    drawNikhil()
